bf1cc2b55ee7e2974f425d04edcc522d1b1c5c0e
Admin <ge1983@yandex.ru>
2023-02-28 17:13:21 +1000

    Добавлено задача 1 для ДЗ

---
+++ DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Number=1;
+
+while (Number <=100) 
+{
+    Console.Write("Ввелите трехзначное число: ");
+    Number = Convert.ToInt32(Console.ReadLine()); 
+    if (Number <=100)
+    {
+        Console.WriteLine("Это не трехзначное число");
+    }
+    else
+    {
+        int Digit = (Number % 100) / 10;
+        Console.WriteLine($"Вторая цифра числа {Number} - {Digit}");
+    }
+}
