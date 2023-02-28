a973667ae963628e8b86e72a23ea78f820e24ca4
Admin <ge1983@yandex.ru>
2023-02-28 17:21:16 +1000

    Добавлено задача 2 для ДЗ

---
+++ DZ2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Ввелите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int Digit = (Number / 100) % 10;
+
+if (Digit == 0)
+{
+    Console.WriteLine($"У числа {Number} - нет третьей цифры");
+} 
+else
+{
+    Console.WriteLine($"Третья цифра числа {Number} - {Digit}");
+}      
